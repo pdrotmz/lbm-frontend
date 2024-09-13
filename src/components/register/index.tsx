9a845f41bde486cda0dfb06b8a4ce26508bc6532
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-12 23:32:41 -0300

    add required, welcome to LBM and a button

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -3,11 +3,12 @@ import './style.scss'
 const RegisterForm = () => {
   return (
     <div className="main">
-          <p>Bem-vindo</p>
+          <p>Welcome to LBM!</p>
           <form action="" method="post" className='form'>
-              <input type="text" placeholder="Username" />
-              <input type="email" placeholder="E-mail" />
-              <input type="password" placeholder="Password" />
+              <input type="text" placeholder="Username" required/>
+              <input type="email" placeholder="E-mail" required/>
+              <input type="password" placeholder="Password" required/>
+              <button type='submit'>Register</button>
           </form>
     </div>
   )
