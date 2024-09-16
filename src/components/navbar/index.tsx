75fe9b0b060bfa99880465ed00dc1d309ca8905e
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-15 21:11:01 -0300

    add navigate to upload video url

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 import './style.scss'
 
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const redirectToUploadVideo = () => {
+    navigate('/teacher-area/upload-video');
+  }
+
   return (
     <>
         <nav>
             <header>
                 <ul>
-                    <li><a href="">Postar Video </a></li>
+                    <li><a onClick={redirectToUploadVideo}>Postar Video </a></li>
                     <li><a href="">Meus Videos</a></li>
                     <li><a href="">Editar Video</a></li>
                     <li><a href="">Excluir Video</a></li>
